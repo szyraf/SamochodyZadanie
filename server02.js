94769d9021e95f31ae97697590b5be0c4e2cbc52
Your Name <you@example.com>
2021-12-01 01:32:19 +0100

    port

--- server02.js
+++ server02.js
@@ -1,6 +1,6 @@
 var express = require("express")
 var app = express()
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 var path = require("path")
 var hbs = require('express-handlebars');
